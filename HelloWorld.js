fc950628eacbbb3c8d8911a3cc69f1ca3aba3579
EldraPratama <76528642+EldraPratama@users.noreply.github.com>
2022-07-06 10:11:52 +0700

    Add files via upload

---
+++ HelloWorld.js
@@ -0,0 +1,5 @@
+const name   = "Eldra Surya Pratama"
+const umur   = 22
+const alamat = "Jl Kerkof No 24 , Leuwigajah , Cimahi"
+console.info("Halo nama saya " + name + " Umur saya " + umur + " tahun ") 
+console.info("Saya tinggal di " + alamat ) 
